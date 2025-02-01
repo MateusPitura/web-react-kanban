2aa9979ea4904c772d1765a10f5a8c5e00f386e2
MateusPitura <mateuspitura@gmail.com>
2025-02-01 18:44:35 -0300

    fix: clean event listeners when drag but don't drop

--- frontend/src/components/Board.tsx
+++ frontend/src/components/Board.tsx
@@ -1,16 +1,17 @@
-import { useContext, useEffect, useMemo, useState } from "react";
+import { useContext, useEffect, useMemo, useRef, useState } from "react";
 import { Context } from "../context/Context";
 import Column from "./Column";
 import { Status } from "../types/model";
 import { fetchAllPosts, updatePost } from "../utils/queries";
 
-const filterStatus = (status: string) => {
+const filterStatus = (status?: string) => {
   return Object.entries(Status).filter(([, value]) => value !== status);
 };
 
 export default function Board() {
   const { posts, selectedFilter, setPosts } = useContext(Context);
   const [currentFakePost, setCurrentFakePost] = useState<string>();
+  const currentPostId = useRef<string | null>(null);
 
   const postsBacklog = [];
   const postsToDo = [];
@@ -31,68 +32,70 @@ export default function Board() {
     else if (post.status === Status.DONE) postsDone.push(post);
   }
 
+  const handleCleanDrop = () => {
+    const statusFiltered = filterStatus();
+    for (const [, value] of statusFiltered) {
+      const pointerEventNoneArea = document.getElementById(
+        `pointerEventNoneArea-${value}`
+      );
+      if (pointerEventNoneArea) {
+        pointerEventNoneArea.style.pointerEvents = "auto";
+      }
+      const columnToDrop = document.getElementById(value);
+      columnToDrop?.removeEventListener("drop", handleDrop);
+      columnToDrop?.removeEventListener("dragenter", handleDragenter);
+      columnToDrop?.removeEventListener("dragover", handleDragover);
+    }
+    setCurrentFakePost(undefined);
+  };
+
+  const handleDragover = async (event: DragEvent) => {
+    event.preventDefault();
+  };
+
+  const handleDragenter = async (event: DragEvent) => {
+    event.preventDefault();
+    if (event.target) {
+      setCurrentFakePost((event.target as HTMLDivElement).id);
+    }
+  };
+
+  const handleDrop = async (event: DragEvent) => {
+    event.preventDefault();
+    if (!event.target) return;
+    const columnTargetDrop = (event.target as HTMLDivElement).id;
+    if (columnTargetDrop && currentPostId.current) {
+      await updatePost(currentPostId.current, columnTargetDrop as Status);
+      const posts = await fetchAllPosts();
+      setPosts(posts);
+    }
+
+    handleCleanDrop();
+  };
+
   useEffect(() => {
     const board = document.getElementById("board");
     board?.addEventListener("dragstart", (event) => {
-      if (event.target) {
-        const [status, id] = (event.target as HTMLDivElement).id.split("-");
+      if (!event.target) return;
 
-        const handleCleanDrop = () => {
-          const statusFiltered = filterStatus(status);
-          for (const [, value] of statusFiltered) {
-            const pointerEventNoneArea = document.getElementById(
-              `pointerEventNoneArea-${value}`
-            );
-            if (pointerEventNoneArea) {
-              pointerEventNoneArea.style.pointerEvents = "auto";
-            }
-            const columnToDrop = document.getElementById(value);
-            columnToDrop?.removeEventListener("drop", handleDrop);
-            columnToDrop?.removeEventListener("dragenter", handleDragenter);
-            columnToDrop?.removeEventListener("dragover", handleDragover);
-          }
-          setCurrentFakePost(undefined);
-        };
+      const [status, id] = (event.target as HTMLDivElement).id.split("-");
+      currentPostId.current = id;
 
-        const handleDragover = async (event: DragEvent) => {
-          event.preventDefault();
-        };
-
-        const handleDragenter = async (event: DragEvent) => {
-          event.preventDefault();
-          if(event.target){
-            setCurrentFakePost((event.target as HTMLDivElement).id);
-          }
-        };
-
-        const handleDrop = async (event: DragEvent) => {
-          event.preventDefault();
-          if (!event.target) return;
-          const columnTargetDrop = (event.target as HTMLDivElement).id;
-          if (columnTargetDrop) {
-            await updatePost(id, columnTargetDrop as Status);
-            const posts = await fetchAllPosts();
-            setPosts(posts);
-          }
-
-          handleCleanDrop();
-        };
-
-        const statusFiltered = filterStatus(status);
-        for (const [, value] of statusFiltered) {
-          const pointerEventNoneArea = document.getElementById(
-            `pointerEventNoneArea-${value}`
-          );
-          if (pointerEventNoneArea) {
-            pointerEventNoneArea.style.pointerEvents = "none";
-          }
-          const columnToDrop = document.getElementById(value);
-          columnToDrop?.addEventListener("drop", handleDrop);
-          columnToDrop?.addEventListener("dragenter", handleDragenter);
-          columnToDrop?.addEventListener("dragover", handleDragover);
+      const statusFiltered = filterStatus(status);
+      for (const [, value] of statusFiltered) {
+        const pointerEventNoneArea = document.getElementById(
+          `pointerEventNoneArea-${value}`
+        );
+        if (pointerEventNoneArea) {
+          pointerEventNoneArea.style.pointerEvents = "none";
         }
+        const columnToDrop = document.getElementById(value);
+        columnToDrop?.addEventListener("drop", handleDrop);
+        columnToDrop?.addEventListener("dragenter", handleDragenter);
+        columnToDrop?.addEventListener("dragover", handleDragover);
       }
     });
+    board?.addEventListener("dragend", handleCleanDrop);
   }, []);
 
   return (
